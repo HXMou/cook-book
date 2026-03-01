092e3c9a84ebc95a9933d283a34d48036eac402b
HXMou <hongxi.mou@gmail.com>
2026-03-01 13:52:14 +0000

    new package sending homework for if and while.

---
+++ if_while.py
@@ -0,0 +1,35 @@
+MAX = 20
+item_weight = int(input("Item weight:\n"))
+package = 0
+total_weight_sent = 0
+package_weight = 0
+package_num = 0
+unsend = 0
+unsend_max = 0
+wast_pasckage = 0
+total_unsend = 0
+
+while item_weight != 0:
+    if 11 > item_weight >0:
+        package_weight += item_weight
+        total_weight_sent += item_weight
+        if package_weight > MAX:
+            package_num += 1
+            unsend = MAX + item_weight - package_weight
+            total_unsend += unsend
+            package_weight = item_weight
+            if unsend >= unsend_max:
+                unsend_max = unsend
+                wast_pasckage = package_num
+            
+        print(f"total weight in current package: {package_weight}, package number:{package_num}, unsend: {unsend}")
+        print(f"total weight of pasckages sent: {total_weight_sent}, total unsent:{total_unsend}")
+        item_weight = int(input("Item weight:\n"))
+    else:
+        print("invalid item weight!")
+        item_weight = int(input("New item weight:\n"))
+
+print(f"Number of package sent: {package_num}.")
+print(f"Total weight of package sent: {total_weight_sent}.")
+print(f"Total unsend capacity: {total_unsend}.")
+print(f"Package number {wast_pasckage} has the most unsend capacity of {unsend_max}")
