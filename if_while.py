09c61348445e6100f001fb6401531ed5d4b833dd
HXMou <hongxi.mou@gmail.com>
2026-03-02 17:24:27 +0000

    ski pair and gondola problem for loop workshop.

--- if_while.py
+++ if_while.py
@@ -2,27 +2,29 @@ MAX = 20
 item_weight = int(input("Item weight:\n"))
 package = 0
 total_weight_sent = 0
-package_weight = 0
+current_package_weight = 0
 package_num = 0
 unsend = 0
 unsend_max = 0
 wast_pasckage = 0
-total_unsend = 0
+total_unsend = 0 
 
 while item_weight != 0:
     if 11 > item_weight >0:
-        package_weight += item_weight
+        currnet_package_weight += item_weight
         total_weight_sent += item_weight
-        if package_weight > MAX:
+
+        if current_package_weight > MAX:
             package_num += 1
-            unsend = MAX + item_weight - package_weight
+            unsend = MAX + item_weight - current_package_weight
             total_unsend += unsend
-            package_weight = item_weight
+            current_package_weight = item_weight
+            
             if unsend >= unsend_max:
                 unsend_max = unsend
                 wast_pasckage = package_num
             
-        print(f"total weight in current package: {package_weight}, package number:{package_num}, unsend: {unsend}")
+        print(f"total weight in current package: {current_package_weight}, package number:{package_num}, unsend: {unsend}")
         print(f"total weight of pasckages sent: {total_weight_sent}, total unsent:{total_unsend}")
         item_weight = int(input("Item weight:\n"))
     else:
--- loops_workshop.py
+++ loops_workshop.py
@@ -4,6 +4,9 @@ num_skiers = int(input("number of skiers: "))
 total_ski_pairs = 0
 current_ski_pairs = 0
 gondola_count = 1
+max_ski_pair = 1
+gondola_with_max_skis = 0
+
 for i in range(num_skiers):
     ski_pairs = random.randint(1, 3)
     print(f"Skier numbe {i+1} has {ski_pairs} pairs of ski, entering gondola number {gondola_count}") #cuz for loop starts from 0
@@ -12,9 +15,16 @@ for i in range(num_skiers):
 
     if current_ski_pairs + ski_pairs > 10:
         print(f"Gondola is full with {gondola_count} gondolas, with {current_ski_pairs} ski pairs")
+
+        if current_ski_pairs > max_ski_pair:
+            max_ski_pair = current_ski_pairs
+            gondola_with_max_skis = gondola_count
+
         gondola_count += 1
         current_ski_pairs = ski_pairs
     else:
         current_ski_pairs += ski_pairs
 
 print(f"total number of ski pairs: {total_ski_pairs}")
+print(f"Gondola with the most skis: {gondola_with_max_skis}")
+print(f"Max ski pairs in a gondola: {max_ski_pair}")
